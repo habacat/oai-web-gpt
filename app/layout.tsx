6a73bd9d6f6ceb9063933e14575bd0ba3c1f4243
habacat <pb_hbc@163.com>
2024-04-12 19:40:51 +0800

    latex fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import AppContextProvider from "@/components/AppContext"
 import EventBusContextProvider from "@/components/EventBusContext"
 import "@/styles/globals.css"
 import "@/styles/markdown.css"
+import 'katex/dist/katex.min.css';
 import type { Metadata } from "next"
 
 export const metadata: Metadata = {
